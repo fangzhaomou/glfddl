c70a9b5c4b142c55a2d0cccaeadacf5f38bcd6c8
fangzhaomou <1079336302@qq.com>
2017-07-07 10:10:17 +0800

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 module.exports = {
 	entry: {
@@ -10,7 +11,8 @@ module.exports = {
 		inverter: './src/js/inverter.js',
 		industrialiot: './src/js/industrialiot.js',
 		projects: './src/js/projects.js',
-		contactus:'./src/js/contactus.js'
+		contactus:'./src/js/contactus.js',
+		vendor: ['element-ui']
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -70,53 +72,62 @@ module.exports = {
 		}]
 	},
 	plugins: [
-		/*new webpack.optimize.UglifyJsPlugin({
+		new webpack.optimize.UglifyJsPlugin({
 		    compress: {
 		        warnings: false,
 		        drop_console: true
 		    }
-		}),*/
+		}),
+		new CommonsChunkPlugin({
+		  name: "vendor",
+		
+		  // filename: "vendor.js"
+		  // (Give the chunk a different name)
+		  minChunks: Infinity,
+		  // (with more entries, this ensures that no other module
+		  //  goes into the vendor chunk)
+		}),
 		new HtmlWebpackPlugin({
 			title: 'home',
 			filename: 'home.html',
 			template: 'src/home.html',
-			chunks: ['home']
+			chunks: ['home','vendor']
 		}),
 		new HtmlWebpackPlugin({
 			title: 'cable',
 			filename: 'cable.html',
 			template: 'src/cable.html',
-			chunks: ['cable']
+			chunks: ['cable','vendor']
 		}),
 		new HtmlWebpackPlugin({
 			title: 'battery',
 			filename: 'battery.html',
 			template: 'src/battery.html',
-			chunks: ['battery']
+			chunks: ['battery','vendor']
 		}),
 		new HtmlWebpackPlugin({
 			title: 'inverter',
 			filename: 'inverter.html',
 			template: 'src/inverter.html',
-			chunks: ['inverter']
+			chunks: ['inverter','vendor']
 		}),
 		new HtmlWebpackPlugin({
 			title: 'industrialiot',
 			filename: 'industrialiot.html',
 			template: 'src/industrialiot.html',
-			chunks: ['industrialiot']
+			chunks: ['industrialiot','vendor']
 		}),
 		new HtmlWebpackPlugin({
 			title: 'projects',
 			filename: 'projects.html',
 			template: 'src/projects.html',
-			chunks: ['projects']
+			chunks: ['projects','vendor']
 		}),
 		new HtmlWebpackPlugin({
 			title: 'contactus',
 			filename: 'contactus.html',
 			template: 'src/contactus.html',
-			chunks: ['contactus']
+			chunks: ['contactus','vendor']
 		})
 	]
 };
